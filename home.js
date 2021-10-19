1a5d682d9d13786f4fe28d8b1987a8d19ee3c33f
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-10-19 07:57:21 +0700

    add even

--- home.js
+++ home.js
@@ -1,9 +1,10 @@
 const introDiv = document.querySelector('.intro')
+const divP = document.querySelector('.intro p')
 console.log(introDiv);
 introDiv.addEventListener('mouseover', (e) => {
-  introDiv.style = 'font-size: 20px';
+  divP.style = 'font-size: 20px';
   introDiv.addEventListener('mouseout', (e) => {
-    introDiv.style = '';
+    divP.style = '';
   })
 
 })  
